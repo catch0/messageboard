3fc072cdc3d3e101aac7c6dfbafdfb248252a834
Spence Peacock <spencerpeacock@gmail.com>
2017-07-24 11:51:08 -0500

    initial commit

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module'
+
+//Components
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MessageBoardComponent } from './message-board/message-board.component';
+import { MessageComponent } from './message-board/message/message.component';
+import { CommentComponent } from './message-board/message/comment/comment.component';
+import { NewCommentComponent } from './message-board/message/new-comment/new-comment.component';
+
+//Services
+import { UserService } from './user.service';
+import { MessageService } from './message.service';
+import { CommentService } from './comment.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MessageBoardComponent,
+    MessageComponent,
+    CommentComponent,
+    NewCommentComponent,
+    MessageBoardComponent,
+    MessageComponent,
+    CommentComponent,
+    NewCommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    UserService,
+    MessageService,
+    CommentService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
